7776171c10a78fd14f466a4f86077c65103b254f
Tudor Dan <tudor.ist@gmail.com>
2026-02-02 20:25:35 +0200

    Add Search Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-// /src/App.tsx`
+// /src/App.tsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './app/Navbar';
 import HomePage from './app/HomePage';
+import SearchPage from './app/SearchPage';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
